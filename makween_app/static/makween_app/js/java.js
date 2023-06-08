b90597efb6848c53ac852b298b9417b8471e24e8
nacha carvajal <ma.carvajalv@duocuc.cl>
2023-06-08 16:43:20 -0400

    validacion contraseña

--- makween_app/static/makween_app/js/java.js
+++ makween_app/static/makween_app/js/java.js
@@ -36,11 +36,38 @@ $("#correoId").keyup(function(){
             "color":"green",
             "fontSize":"10px"
         });
+        $("#correoId").removeClass("is-valid").addClass("is-invalid");
       } else {
         $("#parrafoId1").text("Correo electrónico erróneo.");
         $("#parrafoId1").css({
             "color":"red",
+            "fontSize":"10px"   
+        });
+        $("#correoId").removeClass("is-invalid").addClass("is-valid");
+      }    
+});
+
+//validacion contraseña
+$("#contraseñaId").keyup(function(){
+    var pass = $("#contraseñaId").val();
+    var hasUpperCase = /[A-Z]/.test(pass);
+    var hasLowerCase = /[a-z]/.test(pass);
+    var hasNumber = /\d/.test(pass);
+    var hasSpecialChar = /[!@#$%^&*]/.test(pass);
+    var isValid = pass.length >= 6 && pass.length <= 12 && hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar;
+    if (!isValid) {
+        $("#parrafoId2").text("La contraseña debe tener entre 6 y 12 caracteres y debe incluir una mayúscula, una minúscula, al menos un número y un caracter especial (!@#$%^&*).");
+        $("#parrafoId1").css({
+            "color":"red",
             "fontSize":"10px"
         });
-      }    
+        $("#contraseñaId").removeClass("is-valid").addClass("is-invalid");
+    } else {
+        $("#parrafoId2").text("Contraseña agregada correctamente.");
+        $("#parrafoId1").css({
+            "color":"green",
+            "fontSize":"10px"
+        });
+        $("#contraseñaId").removeClass("is-invalid").addClass("is-valid");
+    }
 });
