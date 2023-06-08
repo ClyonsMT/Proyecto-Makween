43871b2e041b648605b4f5c4ed06cb96d29645ca
matias <matiasriquelme633@dgmail.com>
2023-06-07 23:46:54 -0400

    agregando base de datos sqlite tabla usuarios y cree super usuario

--- makween_app/models.py
+++ makween_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Usuarios(models.Model):
+    nombre_completo = models.CharField(max_length=70)
+    Correo = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre_completo
