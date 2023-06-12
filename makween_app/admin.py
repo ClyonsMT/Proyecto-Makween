03fc4562ff2176a3eb12136d2dc4f14a52ee4788
matias <matiasriquelme633@gmail.com>
2023-06-11 21:54:43 -0400

    creacion de tabla contacto

--- makween_app/admin.py
+++ makween_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Usuarios
+from .models import Contactos
 # Register your models here.
 admin.site.register(Usuarios)
+admin.site.register(Contactos)
--- makween_app/models.py
+++ makween_app/models.py
@@ -7,3 +7,12 @@ class Usuarios(models.Model):
     password = models.CharField(max_length=50)
     def __str__(self):
         return self.nombre_completo
+
+class Contactos(models.Model):
+    Correo = models.CharField(max_length=50)
+    nombre_completo = models.CharField(max_length=70)
+    rut = models.CharField(max_length=70)
+    telefono = models.CharField(max_length=12)
+    mensaje = models.CharField(max_length=400)
+    def __str__(self):
+        return self.rut
