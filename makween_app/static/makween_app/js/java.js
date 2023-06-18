6b4ffadd053ff231fc25567c6e6292df455f9a3a
Cristobal León Merino Tobar <clmerinotobar@gmail.com>
2023-06-18 19:52:50 -0400

    Agregando validaciones al formulario contacto

--- makween_app/static/makween_app/js/java.js
+++ makween_app/static/makween_app/js/java.js
@@ -116,12 +116,74 @@ $("#correoLoginId").keyup(function(){
       }    
 });
 
+//-----------------------------Validaciones del forlulario de contacto-------------------------------
+//Correo electrónico
+$("#correoIdEmailCont").keyup(function(){
+    var correoLogin = $("#correoIdEmailCont").val();
+    var regexLogin = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (regexLogin.test(correoLogin)) {
+        $("#emailIdCon").text("Correo electrónico válido.");
+        $("#emailIdCon").css({
+            "color":"green",
+            "fontSize":"10px"
+        });
+        $("#correoIdEmailCont").removeClass("is-invalid").addClass("is-valid");
+      } else {
+        $("#emailIdCon").text("Correo electrónico erróneo.");
+        $("#emailIdCon").css({
+            "color":"red",
+            "fontSize":"10px"   
+        });
+        $("#correoIdEmailCont").removeClass("is-valid").addClass("is-invalid");
+      }    
+});
+
+//Nombre y apellidos
+$("#nombreIdCont").keyup(function(){
+    var cantidad = $("#nombreIdCont").val().length;
+    if(cantidad < 10 || cantidad > 50){
+        if(cantidad < 10){
+            $("#pNombreCont").text("Debe ingresar al menos 10 caracteres.");
+            $("#pNombreCont").css({
+                "color":"red",
+                "fontSize":"10px"
+            });
+            $("#nombreIdCont").removeClass("is-valid").addClass("is-invalid");
+        }else{
+            $("#pNombreCont").text("Debe ingresar hasta 50 caracteres.");
+            $("#pNombreCont").css({  
+                "color":"red",
+                "fontSize":"10px"
+            });
+            $("#nombreIdCont").removeClass("is-valid").addClass("is-invalid");
+        }
+    }else{
+        $("#pNombreCont").text("Nombre y apellidos agregados correctamente.");
+        $("#pNombreCont").css({   
+            "color":"green",
+            "fontSize":"10px"});
+        $("#nombreIdCont").removeClass("is-invalid").addClass("is-valid");
+    }
+});
+
 //Validacion del rut
+function formatearRut(rut) {
+    rut = rut.replace(/[^\dKk]/g, '');
+    rut = rut.replace(/\./g, '');
+    const dv = rut.slice(-1);
+    const cuerpo = rut.slice(0, -1);
+    var rutFormateado = cuerpo.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+    rutFormateado += '-' + dv.toUpperCase();
+    return rutFormateado;
+}
+
 $("#rutId").blur(function(){
     var rut = $("#rutId").val();
-    const formatoRut = rut.replace(/^(\d{1,2})(\d{3})(\d{3})([\dkK])?$/, '$1.$2.$3-$4');
-    $("#rutId").val(formatoRut);
-    if (formatoRut.test(rut)) {
+    var largo = $("#rutId").val().length;
+    const formatoRut = /(\d{1,3}(?:\.\d{1,3}){2}-[\dkK])|(\d{8}[-][\dkK])/;
+    rut = formatearRut(rut);
+    if (formatoRut.test(rut) && largo == 9) {
+        $("#rutId").val(rut);
         $("#parrafoRut").text("Rut ingresado correctamente.");
         $("#parrafoRut").css({
             "color":"green",
@@ -129,11 +191,31 @@ $("#rutId").blur(function(){
         });
         $("#rutId").removeClass("is-invalid").addClass("is-valid");
     } else {
-        $("#parrafoRut").text("Rut no válido.");
+        $("#parrafoRut").text("Rut no válido");
         $("#parrafoRut").css({
             "color":"red",
             "fontSize":"10px"   
         }); 
         $("#rutId").removeClass("is-valid").addClass("is-invalid");
+    }
+});
+
+//Numero de telefono
+$("#numeroIdCont").keyup(function(){
+    var largo = $("#numeroIdCont").val().length;
+    if (largo == 9) {
+        $("#numeroIdCont").removeClass("is-invalid").addClass("is-valid");
+    } else {
+        $("#numeroIdCont").removeClass("is-valid").addClass("is-invalid");
+    }
+});
+
+//Mensaje
+$("#mensajeCont").keyup(function(){
+    var largo = $("#mensajeCont").val().length;
+    if (largo >= 30) {
+        $("#mensajeCont").removeClass("is-invalid").addClass("is-valid");
+    } else {
+        $("#mensajeCont").removeClass("is-valid").addClass("is-invalid");
     }
 });
